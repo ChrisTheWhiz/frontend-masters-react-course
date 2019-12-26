15c66c076ac7aadec33c2fdcb466970a4004cc06
ChrisTheWhiz <piersinaru.cristian@gmail.com>
2019-12-26 21:34:30 +0200

    implemented babel for modern js, a carousel component, and error boundaries

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,52 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
-const Details = (props) => {
-  // return <h1>hi lol</h1>;
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     loading: true
+  //   };
+  // }
+  state = { loading: true };
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city} - ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        debug: animal,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name, media } = this.state;
+
+    return (
+      <ErrorBoundary>
+        <div className="details">
+          <Carousel media={media} />
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            <p>{description}</p>
+          </div>
+        </div>
+      </ErrorBoundary>
+    );
+  }
+}
 
 export default Details;
